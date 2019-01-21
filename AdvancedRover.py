c17cf03132318b6cde263fc7802024a495f58f99
BCata <bcatalin95@gmail.com>
2019-01-21 19:01:20 +0100

    Implemented report generation + minor code changes for building up the statistics needed

--- AdvancedRover.py
+++ AdvancedRover.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+import sys
 import threading
+from math import floor
 
 from time import time
 from ev3dev2.sound import Sound
@@ -11,7 +12,7 @@ from api.mission_report import generate_mission_report
 
 from api.touch import detect_touch, get_touch_encountered, set_touch_encountered
 from api.parking import park_rover
-from api.measurements import measure_lake
+from api.measurements import measure_lake, get_measurements
 from api.wheel_movement import move_both, stop_both, move_both_for_seconds
 from api.color import color_collision_protocol, detect_color, detect_line
 from api.color import get_right_sensor, get_left_sensor, get_middle_sensor
@@ -27,9 +28,9 @@ GREEN = get_green()
 YELLOW = get_yellow()
 BLACK = get_black()
 
-GLOBAL_TIMEOUT = 2
+GLOBAL_TIMEOUT = 10
 FIND_LAKES_TIMEOUT = 200
-PUSH_BRICKS_TIMEOUT = 250
+PUSH_BRICKS_TIMEOUT = 1
 
 # value = 0
 lakes_found = False
@@ -40,7 +41,8 @@ color_found = {
 }
 
 bricks_pushed = False
-bricks_to_push = 1
+initial_number_of_bricks = 1
+bricks_to_push = initial_number_of_bricks
 
 s = Sound()
 cs_left = get_left_sensor()
@@ -54,17 +56,21 @@ def process_lake(color_val, color_name):
     color_found[color_val] = True
 
     # set mission as completed if all lakes are found
-    global lakes_found
+    global lakes_found, find_lakes_duration
     lakes_found = all(list(color_found.values()))
+
+    if lakes_found:
+        find_lakes_duration = floor(time() - globalStart)
 
 
 def detect_lakes(color_sensor_tuple, lakes):
     color_sensor_firing = [False, False, False]
     has_time_elapsed = time() - globalStart >= FIND_LAKES_TIMEOUT
 
-    global lakes_found
+    global lakes_found, find_lakes_duration
     if has_time_elapsed:
         lakes_found = True
+        find_lakes_duration = floor(time() - globalStart)
 
     for index, color in enumerate(color_sensor_tuple):
         if color in lakes:
@@ -88,12 +94,12 @@ def mission_ongoing():
 
     # global timeout exceeded
     if time() - globalStart >= GLOBAL_TIMEOUT:
-        mission_duration = time() - globalStart
+        mission_duration = floor(time() - globalStart)
         return False
 
     # if all missions completed
     if lakes_found and bricks_pushed:
-        mission_duration = time() - globalStart
+        mission_duration = floor(time() - globalStart)
         return False
 
     return True
@@ -104,7 +110,7 @@ previous_ultrasonic_distance = 0
 
 
 def push_bricks():
-    global bricks_to_push, bricks_pushed, previous_ultrasonic_distance, current_ultrasonic_distance
+    global bricks_to_push, bricks_pushed, previous_ultrasonic_distance, current_ultrasonic_distance, push_bricks_duration
 
     has_time_elapsed = time() - globalStart >= PUSH_BRICKS_TIMEOUT
 
@@ -115,6 +121,7 @@ def push_bricks():
         if bricks_to_push == 0 or has_time_elapsed:
             # done pushing bricks
             bricks_pushed = True
+            push_bricks_duration = floor(time() - globalStart)
             ultrasonic_collision_protocol()
         else:
             # prepare decrement number of bricks after brick falls
@@ -136,14 +143,18 @@ def push_bricks():
 
 def create_mission_report():
     timeouts = {'global_timeout': GLOBAL_TIMEOUT,
-                'global_time_elapseed': mission_duration,
+                'global_time_elapsed': mission_duration,
                 'find_lakes_timeout': FIND_LAKES_TIMEOUT,
-                'find_lakes_elapsed_time': None,
+                'find_lakes_elapsed_time': min(find_lakes_duration, mission_duration),
                 'push_bricks_timeout': PUSH_BRICKS_TIMEOUT,
-                'push_bricks_elapsed_time': None
+                'push_bricks_elapsed_time': min(push_bricks_duration, mission_duration)
                 }
 
-    measurements = {}
+    measurements = {'lakes': get_measurements(),
+                    'bricks_pushed': "{0} out of {1}".format(initial_number_of_bricks - bricks_to_push,
+                                                             initial_number_of_bricks),
+                    'celebrate': 'sing'
+                    }
 
     generate_mission_report(timeouts, measurements)
 
@@ -155,7 +166,9 @@ if __name__ == "__main__":
 
     lakes_to_find = (RED, GREEN, BLUE)
 
-    mission_duration = -1
+    mission_duration = sys.maxsize
+    find_lakes_duration = sys.maxsize
+    push_bricks_duration = sys.maxsize
 
     # start timers
     globalStart = time()
@@ -172,12 +185,12 @@ if __name__ == "__main__":
     stop_both()
     s.speak("Exploration finished")
 
-    # s.speak("Let us celebrate")
-    # celebrate("sing")
-    #
-    # s.speak("Now let me park")
-    # park_rover(BORDER_COLOR)
-    # stop_both()
+    s.speak("Let us celebrate")
+    celebrate("sing")
+
+    s.speak("Now let me park")
+    park_rover(BORDER_COLOR)
+    stop_both()
 
     create_mission_report()
     s.speak("Mission report generated")
